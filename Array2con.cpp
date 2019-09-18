21707c2941c469839d958a4c04646d2c7fd40daa
sptakyw <52157614+sptakyw@users.noreply.github.com>
2019-09-18 10:03:41 +0700

    MATRIX 2*2

---
+++ Array2con.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int A[2][2],B[2][2];
+		cout<< "Enter matrix A :" << endl;
+	for(int i=0;i<2;i++) {
+		cin>> A[i][0] >> A[i][1];
+}
+
+	cout<< "Enter matrix B :" << endl;
+	for(int i=0;i<2;i++) 
+		{
+			cin>> B[i][0] >> B[i][1];
+		}
+
+	cout<< "Matrix A+B :" << endl;
+
+	for(int i=0;i<2;i++) 
+	{
+		for(int j=0;j<2;j++)
+
+		{ cout << A[i][j] + B[i][j] << " " ; }
+	cout << endl;
+
+	}
+return 0;
+}
