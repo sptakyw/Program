77a7baedfcd89a3b5d342ad5f6a28340a634f089
sptakyw <52157614+sptakyw@users.noreply.github.com>
2019-09-18 10:27:09 +0700

    Add files via upload

---
+++ MATRIX AND POW2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+void display (int nums[3][4]); 
+void pow2 (int nums[3][4]);
+
+int main()
+{	int val[3][4] = {8,16,9,52,3,15,27,6,14,25,2,10};
+	display(val) ;
+	pow2(val) ;
+	return 0;
+}
+
+void display (int nums[3][4])
+	{
+		for(int row_num = 0 ; row_num < 3 ; row_num++) 
+		{
+			for(int col_num = 0; col_num < 4; ++col_num)
+				cout << nums[row_num][col_num] <<"\t";
+			cout << endl;
+		} 
+cout << "******* value * value ******* " << endl;
+
+	}
+
+void pow2  (int nums[3][4])
+	{
+		for(int row_num = 0 ; row_num < 3 ; row_num++) 
+		{
+			for(int col_num = 0; col_num < 4; ++col_num)
+				cout << nums[row_num][col_num] * nums[row_num][col_num] <<"\t";
+			cout << endl;
+		}
+	}
